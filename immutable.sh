2b981936c60fcf3edb25776b66370720cb8e004d
Samorizu <jps5374@rit.edu>
2013-10-05 18:29:22 -0400

    adding chmod

--- immutable.sh
+++ immutable.sh
@@ -8,12 +8,14 @@ if [ $OPTION -eq "+i" ]
    then
       sudo chattr +i $FILE
       echo "Added +i to $FILE"
+      chmod 000 $FILE
       
 #removing the -i
 elif [ $OPTION -eq "-i" ]
    then
       sudo chattr -i $FILE
       echo "Added -i to $FILE"
+      chmod 700 $FILE
 
 #else, fail
 else
