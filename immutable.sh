7cc9df57fb712493f35a3cb7537c7f409b22740c
Jackson Sadowski <samorizu@users.noreply.github.com>
2013-10-05 05:42:48 -0400

    Create immutable.sh

---
+++ immutable.sh
@@ -0,0 +1,22 @@
+#making a file immunable
+
+read -p "File location: " FILE
+read -p "Do you want to +i or -i? " OPTION
+
+#adding the +i
+if [ $OPTION -eq "+i" ]
+   then
+      sudo chattr +i $FILE
+      echo "Added +i to $FILE"
+      
+#removing the -i
+elif [ $OPTION -eq "-i" ]
+   then
+      sudo chattr -i $FILE
+      echo "Added -i to $FILE"
+
+#else, fail
+else
+   echo "Failure."
+
+fi
