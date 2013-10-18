32be277af20990284976e56ffaafd6f2c9c4804e
bigshebang <ljm3103@rit.edu>
2013-10-17 20:34:05 -0400

    adding some error checking features to make script more robust. also added package manager detection

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 #BEFORE RUNNING THIS SCRIPT
 #Text files needed:
 #iptables.sh - will be a script containing the iptables rules you want
-#netconfig.txt - containing the interface config
+#netconfig.txt - containing the interface config - must be changed to match network setup
 #
 ####################################################################################
 #THINGS TO CHECK ######################################
@@ -18,13 +18,13 @@ fi
 #setting the net int down
 ifconfig down $1
 
-outfile=info.txt
+outfile=info.txt #set output file
 
 #cronjobs aka blowjob
 crontab -r
 
-#getting all the ip tables rules
-./iptables.sh
+#calling iptables script to set all the ip tables rules
+./iptables.sh &
 
 #set static ip address and do network interface config stuff
 netconfig="/etc/network/interfaces"
@@ -48,12 +48,30 @@ chmod 600 $hosts
 #put the interface back up
 ifconfig up $1
 
-#upgrading and updating everything
-apt-get update &
-apt-get upgrade -y &
+#upgrading and updating everything. first determine package manager
+if [ -f /etc/redhat-release ] ; then
+	pkmgr=`which yum`
+elif [ -f /etc/debian_version ] ; then
+	pkmgr=`which apt-get`
+elif [ -f /etc/gentoo_version ]; then #possible might need this too: -f /etc/gentoo-release
+	pkmgr=`which emerge`
+elif [ -f /etc/slackware-version ]; then
+	pkmgr=`which installpkg`
+else
+	pkmgr=`which apt-get` #if can't find OS, just use apt-get and hope for best
+fi
 
-#makes the jail
-./jail_maker.sh -s /var/jail &
+$pkmgr update &
+$pkmgr upgrade -y &
+
+#makes the jail. if /var/jail taken, somewhat random directory attempted to be made in hopes it doesn't exist
+if [ ! -e /var/jail ]; then
+	./jail_maker.sh -s /var/jail &
+elif [ ! -e /var/jm_jail_5186 ]; then
+	./jail_maker.sh -s /var/jm_jail_5186 &
+else
+	echo "jail not made. must pick a new directory"
+fi
 
 #Make Sure No Non-Root Accounts Have UID Set To 0
 echo "Accounts with UID = 0" >> $outfile
@@ -71,12 +89,12 @@ echo "" >> $outfile
 
 #finding all of the world-writeable files
 echo "All of the world-writable files" >> $outfile
-find /dir -xdev -type d \( -perm -0002 -a ! -perm -1000 \) -print >> $outfile
+find / -xdev -type d \( -perm -0002 -a ! -perm -1000 \) -print >> $outfile
 echo "" >> $outfile
 
 #finding all of the no owner files
 echo "All of the no owner files" >> $outfile
-find /dir -xdev \( -nouser -o -nogroup \) -print >> $outfile
+find / -xdev \( -nouser -o -nogroup \) -print >> $outfile
 echo "" >> $outfile
 
 #prompt for ssh box
@@ -87,7 +105,12 @@ if [ "$answer" == "Y" ]; then
 fi
 
 #prompt for web box
-
+#web.sh does not exist yet
+read -p "is this a web box (Y/N): " answer
+answer=`echo $answer | tr '[:lower:]' '[:upper:]'`
+if [ "$answer" == "Y" ]; then
+	./web.sh &
+fi
 
 
 
