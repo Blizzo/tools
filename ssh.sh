2a88ad344b9d17c995ed853a07532eec1787d515
bigshebang <ljm3103@rit.edu>
2013-10-17 21:19:56 -0400

    rearranging and updating comments

--- ssh.sh
+++ ssh.sh
@@ -1,9 +1,11 @@
 #ssh.sh - for box with ssh service on it
 #hardens the ssh config file
 
-#disable root login 
+#store sshd config file location and back it up before changing it
 sshfile="/etc/ssh/sshd_config"
 mv $sshfile $sshfile.backup
+
+#disable root login
 num=`cat $sshfile | grep "PermitRootLogin" -n | cut -f1 -d:`
 sed '${num}d' $sshfile
 echo "" >> $sshfile
