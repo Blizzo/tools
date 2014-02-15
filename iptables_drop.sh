e700e09d335813292314e3c7d51fb9770aa6635c
Jackson Sadowski <samorizu@users.noreply.github.com>
2014-02-15 18:33:59 -0500

    Delete iptables_drop.sh

--- iptables_drop.sh
+++
@@ -1,9 +0,0 @@
-#getting to drop all traffic
-iptable -F
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
-
-# Drop all IPv6 stuff
-ip6tables -A INPUT -j DROP
-ip6tables -A OUTPUT -j DROP
