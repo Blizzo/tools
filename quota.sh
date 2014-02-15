4665172596488d5f0c535bf0c27e6c336850c63a
Luke Matarazzo <ljm3103@rit.edu>
2014-02-15 18:10:43 -0500

    Adding loop functionality

--- quota.sh
+++ quota.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #Setting a disk quota for a user
 
-read -p "Please enter a user: " USER
-if [ "$USER" == "" ]; then
-   echo "User invalid!"
-   exit
-fi
-
 #Installing quota
-sudo apt-get install quota -y
+sudo apt-get install quota -y &
 
-#setting the quota
-#softquota/blocks/softquota/inodes
-setquota $USER 0 512020 0 1000 -a
+while [ true ]; do
+   read -p "Please enter a user (leave blank to exit): " USER
+
+   if [ "$USER" == "" ]; then
+      echo "No user given. Bye!"
+      exit 0
+   fi
+
+   #setting the quota
+   #softquota/blocks/softquota/inodes
+   setquota $USER 0 512020 0 1000 -a
+done
