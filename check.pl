ad758e8e9c2c466a60a690fd77745c088e22c2b9
Ubuntu <ubuntu@ip-10-238-249-243.ec2.internal>
2014-02-17 17:34:59 +0000

    Makes a hash of the the last modified date/time of files and directories

---
+++ check.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+#Jackson Sadowski
+#Directory Monitor
+
+use strict;
+
+my $initialTime = `date +"%Y-%m-%d %T"`;
+my @initialFiles = `ls -l | grep ^\- | cut -d' ' -f10`;
+my @initialDirs = `ls -l | grep ^d | cut -d' ' -f9`;
+
+chomp @initialFiles;
+chomp @initialDirs;
+
+#initializing hashes
+my %files = ();
+my %dirs = ();
+
+#getting the current date of the files
+foreach (@initialFiles){
+   my $date = `stat -c%y $_ | cut -d. -f1 | cut -d' ' -f1-2`;
+   $files{ $_ } = $date;
+}
+
+#getting the current date of the dirs
+foreach (@initialDirs){
+   my $date = `stat -c%y $_ | cut -d. -f1 | cut -d' ' -f1-2`;
+   $dirs{ $_ } = $date;
+}
+
+#printing info
+print "INITIAL = $initialTime";
+while ( my ($key, $value) = each(%files) ) {
+   print "FILE: $key => $value";
+}
+
+#printing files and dates
+while ( my ($key, $value) = each(%dirs) ) {
+   print "DIR: $key => $value";
+}
