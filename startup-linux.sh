c8c30fdadf3b6d59fc9fadfe33b2b73442b9adeb
Luke <ljm3103@rit.edu>
2014-02-23 01:32:47 -0500

    Fixing errors, modernizing
    Getting rid of old stuff and unnecessary stuff as well as fixing errors.

--- startup-linux.sh
+++ startup-linux.sh
@@ -2,6 +2,7 @@
 #MAKE SURE ALL THE TXT FILES ARE CORRECT
 #BEFORE RUNNING THIS SCRIPT
 #must be run as root
+#must change root password first! must!
 #Text files needed:
 #iptables.sh - will be a script containing the iptables rules you want
 #
@@ -19,12 +20,15 @@ if [ $# -ne 1 ]
 fi
 
 #setting the net int down
-/bin/ifconfig down $1
+/sbin/ifconfig $1 down
 
 outfile=info.txt #set output file
 
 #cronjobs aka blowjob
-/usr/bin/crontab -r #remove crontabs
+lines=`/bin/cat /etc/passwd | grep -o '^\w*'`
+for line in $lines; do
+        crontab -r -u $line
+done &> /dev/null
 
 /bin/chown root:root /etc/cron* -R
 /bin/chmod o= /etc/cron* -R
@@ -36,6 +40,7 @@ outfile=info.txt #set output file
 /bin/chmod o= /usr/bin/crontab
 /usr/bin/chattr +i /usr/bin/crontab
 
+/usr/bin/chattr -i /etc/anacrontab
 /bin/chown root:root /etc/anacrontab
 /bin/chmod o= /etc/anacrontab
 /usr/bin/chattr +i /etc/anacrontab
@@ -48,82 +53,56 @@ outfile=info.txt #set output file
 /bin/mv /etc/anacrontab /etc/.anacrontab.bak
 /usr/bin/chattr +i /etc/anacrontab
 
-#edit sudoers
-/bin/mv /etc/sudoers /etc/.sudoers.bak
-echo " " > /etc/sudoers
-/bin/chmod 000 /etc/sudoers
-/usr/bin/chattr +i /etc/sudoers
-
-#calling iptables script to set all the ip tables rules
+#calling iptables script to set all the ip tables rules and add to startup
 ./iptables.sh &
-echo "`pwd`/iptables.sh " | /bin/cat /etc/rc.local > /etc/rc.local
-
-function fix_repo {
-	if [ ! -e $2 ]; then
-		return 1
-	fi
-	if [ "$1" == "apt" ]; then
-		/bin/mv /etc/apt/sources.list /etc/apt/sources.list.bak
-		echo "INSERT REPO LINE HERE" > /etc/apt/sources.list
-		/usr/bin/chattr +i /etc/apt/sources.list
-		/usr/bin/chattr +i /etc/apt
-	elif [ "$1" == "yum" ]; then
-		repos=`ls /etc/yum.repos.d`
-		for f in $repos; do
-			/bin/mv $f $f.bak
-		done
-		echo "INSERT REPO LINE HERE" > /etc/yum.repos.d/default.repo
-		/usr/bin/chattr +i /etc/yum.repos.d/default.repo
-		/usr/bin/chattr +i /etc/yum.repos.d
-	elif [ "$1" == "emerge" ]; then
-		echo "$1 fix_repo feature not yet implemented"
-	fi
-}
+cp /etc/rc.local /etc/rc.local.bak
+/bin/cat /etc/rc.local.bak > /etc/rc.local
+echo "`pwd`/iptables.sh " >> /etc/rc.local
 
 #determine distro to get package manage and int config location
 if [ -f /etc/redhat-release ] ; then
 	pkmgr=`/usr/bin/yum`
-	sys_netconfig="/etc/sysconfig/network-scripts/ifcfg-$1"
-	fix_repo yum repos.txt # do repo stuff
+	#sys_netconfig="/etc/sysconfig/network-scripts/ifcfg-$1"
 elif [ -f /etc/debian_version ] ; then
 	pkmgr=`/usr/bin/apt-get`
-	sys_netconfig="/etc/network/interfaces"
-	fix_repo apt repos.txt # do repo stuff
+	#sys_netconfig="/etc/network/interfaces"
 elif [ -f /etc/gentoo_version ]; then #possible might need this too: -f /etc/gentoo-release
 	pkmgr=`/usr/bin/emerge`
 	/bin/ln -s /etc/init.d/net.lo /etc/init.d/net.$1 #create link so system recognizes net.lo file. needed for manual net config
-	sys_netconfig="/etc/conf.d/net"
-	#still need to figure out
-	fix_repo emerge repos.txt # do repo stuff
+	#sys_netconfig="/etc/conf.d/net"
 elif [ -f /etc/slackware-version ]; then
 	pkmgr=`/usr/bin/which installpkg`
-	sys_netconfig="/etc/rc.d/rc.inet1.conf"
-	fix_repo apt repos.txt # do repo stuff
+	#sys_netconfig="/etc/rc.d/rc.inet1.conf"
 else
 	echo "OS/distro not detected...using debian defaults..." >&2
 	pkmgr=`/usr/bin/apt-get` #if can't find OS, just use apt-get and hope for best
-	sys_netconfig="/etc/network/interfaces"
-	fix_repo apt repos.txt # do repo stuff
+	#sys_netconfig="/etc/network/interfaces"
 fi
 
 #set static ip address, gateway and DNS
-/bin/ifconfig $1 <IP_ADDR> netmask <NET_MASK>
-/bin/route add default gw <GATEWAY_IP>
+/sbin/ifconfig $1 <IP_ADDR> netmask <NET_MASK>
+/sbin/route add default gw <GATEWAY_IP>
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 #echo "nameserver <TEAM_DNS_SRVR>" >> /etc/resolv.conf
-
 
 #set hosts file location and do hosts file securing
 hosts="/etc/hosts"
 
+/usr/bin/chattr -i $hosts
 /bin/cp $hosts $hosts.backup
 echo "127.0.0.1       localhost" > $hosts
 echo "127.0.1.1       `hostname`" >> $hosts
 /usr/bin/chattr +i $hosts
 /bin/chmod 600 $hosts
 
+#edit sudoers
+/bin/mv /etc/sudoers /etc/.sudoers.bak
+echo " " > /etc/sudoers
+/bin/chmod 000 /etc/sudoers
+/usr/bin/chattr +i /etc/sudoers
+
 #put the interface back up ifconfig up $1
-/bin/ifconfig up $1
+/sbin/ifconfig $1 up
 
 #upgrading and updating everything
 $pkmgr update & disown &> .updateinfo.txt
@@ -140,7 +119,7 @@ fi
 
 #Make Sure No Non-Root Accounts Have UID Set To 0
 echo "Accounts with UID = 0" >> $outfile
-echo `/bin/awk -F: '($3 == "0") {print}' /etc/passwd` >> $outfile
+echo `/usr/bin/awk -F: '($3 == "0") {print}' /etc/passwd` >> $outfile
 echo "" >> $outfile
 
 #all listening ports
@@ -168,21 +147,3 @@ echo "" >> $outfile
 /usr/bin/chattr +i /usr/bin/rebootz
 /bin/mv /sbin/shutdown /sbin/shutdownz
 /usr/bin/chattr +i /sbin/shutdownz
-
-#prompt for ssh box
-read -p "is this an ssh box (Y/N): " answer
-answer=`echo $answer | /usr/bin/tr '[:lower:]' '[:upper:]'`
-if [ "$answer" == "Y" ]; then
-	./ssh.sh &
-fi
-
-#prompt for web box
-#web.sh does not exist yet
-read -p "is this a web box (Y/N): " answer
-answer=`echo $answer | /usr/bin/tr '[:lower:]' '[:upper:]'`
-if [ "$answer" == "Y" ]; then
-	./web.sh &
-fi
-
-
-
