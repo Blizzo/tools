9f881970cfac75ca6f4419efb4c9b6b2b017f17d
Luke Matarazzo <ljm3103@rit.edu>
2014-02-23 01:49:09 -0500

    adding freebsd files

---
+++ ipfw.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#set path of ipfw
+path=/sbin
+
+
+
---
+++ startup-bsd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#startup script for freebsd
+#MAKE SURE ALL THE TXT FILES ARE CORRECT
+#BEFORE RUNNING THIS SCRIPT
+#must be run as root
+#Text files needed:
+#ipfw.sh - will be a script containing the iptables rules you want
+
+
+
