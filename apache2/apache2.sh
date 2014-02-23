ec1c3578440ccbd4a51322462cc8ef90bade7ef0
Samorizu <jps5374@rit.edu>
2014-02-23 02:35:26 +0000

    Installs apache with Mod Security and other OWASP features.

--- apache2/apache2.sh
+++ apache2/apache2.sh
@@ -25,11 +25,11 @@ service apache2 restart
 
 #MOD SECURITY SECTOIN
 #installing dependencies
-sudo apt-get install libxml2 libxml2-dev libxml2-utils -y
-sudo apt-get install libaprutil1 libaprutil1-dev -y
+apt-get install libxml2 libxml2-dev libxml2-utils -y
+apt-get install libaprutil1 libaprutil1-dev -y
 
 #installing mod security
-sudo apt-get install libapache-mod-security -y
+apt-get install libapache-mod-security -y
 
 #moving in my config file
 mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf.bak
@@ -41,21 +41,13 @@ tar -zxvf SpiderLabs-owasp-modsecurity-crs.tar.gz
 cp -R SpiderLabs-owasp-modsecurity-crs-*/* /etc/modsecurity/
 rm SpiderLabs-owasp-modsecurity-crs.tar.gz
 rm -R SpiderLabs-owasp-modsecurity-crs-*
-sudo mv /etc/modsecurity/modsecurity_crs_10_setup.conf.example /etc/modsecurity/modsecurity_crs_10_setup.conf
+mv /etc/modsecurity/modsecurity_crs_10_setup.conf.example /etc/modsecurity/modsecurity_crs_10_setup.conf
 
 #creating links
 cd /etc/modsecurity/base_rules
-for f in * ; do sudo ln -s /etc/modsecurity/base_rules/$f /etc/modsecurity/activated_rules/$f ; done
+for f in * ; do ln -s /etc/modsecurity/base_rules/$f /etc/modsecurity/activated_rules/$f ; done
 cd /etc/modsecurity/optional_rules
-for f in * ; do sudo ln -s /etc/modsecurity/optional_rules/$f /etc/modsecurity/activated_rules/$f ; done 
-
-#making the conf file avalible
-rm /etc/apache2/mods-available/mod-security.conf
-echo "<IfModule security2_module>" >> /etc/apache2/mods-available/mod-security.conf
-echo "        SecDataDir /var/cache/modsecurity" >> /etc/apache2/mods-available/mod-security.conf
-echo "        Include \"/etc/modsecurity/activated_rules/*.conf\"" >> /etc/apache2/mods-available/mod-security.conf
-echo "        Include \"/etc/modsecurity/*.conf\"" >> /etc/apache2/mods-available/mod-security.conf
-echo "</IfModule>" >> /etc/apache2/mods-available/mod-security.conf
+for f in * ; do ln -s /etc/modsecurity/optional_rules/$f /etc/modsecurity/activated_rules/$f ; done 
 
 #restarting apache to enable
 service apache2 restart
