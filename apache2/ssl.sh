2bff861f660670c74cdb3326461bb6bb22db58c9
Samorizu <jps5374@rit.edu>
2014-02-26 15:25:59 +0000

    Script that adds SSL to an apache installation

---
+++ apache2/ssl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Setting up SSL on apache2
+
+#ask if they've installed before
+read -p "Have you generated your SSL Cert? [y/n]: " ANS
+
+#if this is the first time
+if [ "$ANS" == "n" ]; then
+   #enabling
+   a2enmod ssl
+   service apache2 restart
+
+   #making a new dir
+   mkdir /etc/apache2/ssl
+   cd /etc/apache2/ssl
+   echo "Run the following command:"
+   echo "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt"
+   exit
+
+#if they've run the script before
+elif [ "$ANS" == "y" ]; then
+   mv /etc/apache2/sites-available/default-ssl /etc/apache2/sites-available/default-ssl.bak
+   mv `pwd`/default /etc/apache2/sites-available/default-ssl
+   a2ensite default-ssl
+   service apache2 reload
+   exit
+
+#if a wrong selection
+else
+   echo "Incorrect selection"
+   exit
+fi
