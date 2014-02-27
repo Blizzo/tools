d58ed573a3863193951afa680aa398e14a3b6a83
Luke Matarazzo <ljm3103@rit.edu>
2014-02-27 18:06:51 -0500

    fixing user mgmt

--- users-linux.sh
+++ users-linux.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 #edit users - change password for all users and disable other accounts
 
-read -p -s "Enter password for all non-privileged users: " pass
-users=`/bin/cat /etc/passwd | grep -o '^\w*'`
-for user in $users; do
+read -s -p "Enter password for all non-privileged users: " pass
+read -s -p "Enter password again: " pass_two
+
+while [ "$pass" != "$pass_two" ]; do
+	echo "Passwords did not match. Try again."
+	read -s -p "Enter password for all non-privileged users: " pass
+	read -s -p "Enter password again: " pass_two
+done
+
+for user in $(awk -F":" '{if($3>=500) print $1}' /etc/passwd); do
 	echo "$user:$pass" | chpasswd
 done
 
 echo "Be careful not to disable a useful/necessary account."
+users=`/bin/cat /etc/passwd | grep -o '^\w*'`
 for user in $users; do
 	if [ "$user" == "root" ]; then
 		continue
