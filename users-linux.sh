4fa69196b292397741d3ac691dabf37923a85593
Luke Matarazzo <ljm3103@rit.edu>
2014-02-27 13:39:10 -0500

    adding user management scripts

---
+++ users-linux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#edit users - change password for all users and disable other accounts
+
+read -p -s "Enter password for all non-privileged users: " pass
+users=`/bin/cat /etc/passwd | grep -o '^\w*'`
+for user in $users; do
+	echo "$user:$pass" | chpasswd
+done
+
+echo "Be careful not to disable a useful/necessary account."
+for user in $users; do
+	if [ "$user" == "root" ]; then
+		continue
+	fi
+	read -p "Do you want to disable '$user' (Y/N): " answer
+	answer=`echo $answer | /usr/bin/tr '[:lower:]' '[:upper:]'`
+	if [ "$answer" == "Y" -o "$answer" == "YES" ]; then
+		usermod $user -s /bin/nologin
+	fi
+done
