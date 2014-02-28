ca584134b742b85141b37f2be7f6eb316b0c639e
Luke <ljm3103@rit.edu>
2014-02-28 10:51:37 -0500

    Update ipfilter.sh

--- ipfilter.sh
+++ ipfilter.sh
@@ -3,16 +3,17 @@
 path=/sbin
 file=/etc/ipf/ipf.conf
 
-#allow icmp
+#allow icmp; block bad stuffz
+echo "block return-icmp(net-unr) in proto udp all" >> $file # return ICMP error packets for invalid UDP packets
 echo "pass in quick proto icmp from any to any  keep state group 100" > $file
 echo "pass out quick proto icmp from any to any keep state group 200" >> $file
 
 #regular rules
 echo "block in log quick all with short" >> $file
-echo "pass in quick proto tcp from any to any port = 80 keep state" >> $file
-echo "pass out quick proto tcp from any to any port = 80 keep state" >> $file
-echo "pass in quick proto tcp from any to any port = 443 keep state" >> $file
-echo "pass out quick proto tcp from any to any port = 443 keep state" >> $file
+echo "pass in quick proto tcp from any to any port = 80 keep state" >> $file #web
+echo "pass out quick proto tcp from any to any port = 80 keep state" >> $file #web
+echo "pass in quick proto tcp from any to any port = 443 keep state" >> $file #web
+echo "pass out quick proto tcp from any to any port = 443 keep state" >> $file #web
 echo "pass out quick proto udp from any to any port = domain keep state" >> $file #dns
 
 
