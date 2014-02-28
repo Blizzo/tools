f8631c4709854fa241dffff055f3e5f916fb03cc
Luke <ljm3103@rit.edu>
2014-02-28 10:46:23 -0500

    Create ipfilter.sh
    ipf rules meant for solaris

---
+++ ipfilter.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+path=/sbin
+file=/etc/ipf/ipf.conf
+
+#allow icmp
+echo "pass in quick proto icmp from any to any  keep state group 100" > $file
+echo "pass out quick proto icmp from any to any keep state group 200" >> $file
+
+#regular rules
+echo "block in log quick all with short" >> $file
+echo "pass in quick proto tcp from any to any port = 80 keep state" >> $file
+echo "pass out quick proto tcp from any to any port = 80 keep state" >> $file
+echo "pass in quick proto tcp from any to any port = 443 keep state" >> $file
+echo "pass out quick proto tcp from any to any port = 443 keep state" >> $file
+echo "pass out quick proto udp from any to any port = domain keep state" >> $file #dns
+
+
+#block all else
+echo "block in from any to any head 100" >> $file
+echo "block out from any to any head 200" >> $file
+
