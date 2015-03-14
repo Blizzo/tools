76921af5d714c8ec236192d94fc964ce310f73a2
Luke Matarazzo <ljm3103@rit.edu>
2015-03-13 23:16:43 -0400

    move renamyBinary to the red team directory

---
+++ red/renameBinary.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+#Randomly rename binaries
+
+$binaryDir = 'rundir/binaries';
+@chars = (a..z, A..Z, 0..9);
+
+@binaries = `ls $binaryDir/`;
+
+foreach (@binaries){
+   chomp $_;
+   $rstr = join '', map { @chars[rand @chars] } 1 .. 8;
+
+   print "$_ changed to $rstr\n";
+   system("mv $binaryDir/$_ $binaryDir/$rstr");
+}
+
